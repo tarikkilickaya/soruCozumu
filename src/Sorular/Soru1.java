72876a915c0e0c9eb1dbb7d6f115756fca289aa3
tarikklckya <trkklckya@gmail.com>
2022-08-19 18:02:35 +0300

    19.08.2022

---
+++ src/Sorular/Soru1.java
@@ -0,0 +1,11 @@
+package Sorular;
+
+import java.util.Scanner;
+
+public class Soru1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sayı gir");
+        int sayi = scan.nextInt();
+    }
+}
